b4c7301f3b63b1b9f16ced4a7bd9b232f827d251
Klement <anselmo.klement.chua@gmail.com>
2024-07-23 19:11:15 +0800

    Enabled calculator functionalty

--- javascript.js
+++ javascript.js
@@ -1,6 +1,44 @@
-let num1;
+let num1 = "";
 let operator;
-let num2;
+let operatorPressed = false;
+let num2 = "";
+let result;
+
+//Event Listeners
+const buttons = document.querySelector(".buttons"); 
+buttons.addEventListener("click", (e) => {
+    if (!operatorPressed) {
+        switch(e.target.className) {
+            case "btn btn-num":
+                num1 += e.target.id;
+                displayResult(num1);
+                break;
+            case "btn btn-op btn-func":
+                operator = e.target.id;
+                operatorPressed = true;
+        }
+    } else {
+        switch(e.target.className) {
+            case "btn btn-num":
+                num2 += e.target.id;
+                displayResult(num2);
+                break;
+            case "btn btn-op btn-equal":
+                result = operate(parseInt(num1),parseInt(num2),operator);
+                displayResult(result);
+        }
+    }
+})
+
+const btnClear = document.querySelector("#ac")
+btnClear.addEventListener("click", () => {
+    num1 = "";
+    num2 = "";
+    result = "";
+    operator = "";
+    operatorPressed = false;
+    displayResult("")
+})
 
 
 // Calculator Functions
@@ -13,11 +51,11 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-    return parseFloat((a * b).toPrecision(10));
+    return parseFloat((a * b).toPrecision(5));
 }
 
 function divide(a, b) {
-    return parseFloat((a / b).toPrecision(10));
+    return parseFloat((a / b).toPrecision(5));
 }
 
 function operate(a, b, operator) {
@@ -28,3 +66,11 @@ function operate(a, b, operator) {
         case "divide": return divide(a,b);
     }
 }
+
+// Display functions
+function displayResult(text) {
+    const resultDisplay = document.querySelector(".result-display");
+    resultDisplay.textContent = `${text}`;
+}
+
+// Event Listener Support Functions
